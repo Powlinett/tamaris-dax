c74fe2bf492036443bbae2931427e6537d3e51dc
Pauline <pauline.eustachy@gmail.com>
2020-05-01 03:44:44 +0200

    initialize_on_precompile: false

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,7 @@ module TamarisDax
 
     config.action_view.embed_authenticity_token_in_remote_forms = true
 
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
