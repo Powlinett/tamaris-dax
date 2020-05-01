94ed48628ccb8e0d30df605dbe2498ddd8ccfd49
Pauline <pauline.eustachy@gmail.com>
2020-05-01 03:37:55 +0200

    booking table continued and bookers list started

--- app/controllers/bookers_controller.rb
+++ app/controllers/bookers_controller.rb
@@ -1,5 +1,6 @@
 class BookersController < ApplicationController
   def index
-    @bookers = Booker.group(:email).order(updated_at: :desc)
+    @bookers = Booker.all.uniq { |booker| booker.email }
+    @bookers = @bookers.sort_by { |booker| booker.created_at }
   end
 end
