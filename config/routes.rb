c15676098f844e8c6ad9449ae8b190bfa8363f45
Pauline <pauline.eustachy@gmail.com>
2020-05-04 21:29:19 +0200

    only one user can be registered

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: "registrations" }
   root to: 'pages#home'
 
   resources :products, param: :reference, only: [:index, :new, :show, :create] do
