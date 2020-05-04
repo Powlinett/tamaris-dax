23170df94c49200ccff907d618aa778dcf9e5b6d
Pauline <pauline.eustachy@gmail.com>
2020-05-04 21:36:08 +0200

    only two users (admin and usual)

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,7 +4,7 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def one_user_registered?
-    return unless User.count == 1
+    return unless User.count == 2
 
     if user_signed_in?
       redirect_to root_path
