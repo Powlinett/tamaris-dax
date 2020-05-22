e0ff13890f439f5988c5157349ae22419a135037
Pauline <pauline.eustachy@gmail.com>
2020-05-22 04:53:06 +0200

    add webpack and start responsive on navbar

---
+++ app/assets/javascripts/components/products_search.js
@@ -0,0 +1,8 @@
+const menu = document.querySelector('.navbar-toggler');
+const mobileList = document.querySelectorAll('.mobile-list');
+
+menu.addEventListener('click', (event) => {
+  mobileList.forEach(function(list){
+    list.classList.toggle
+  });
+});
---
+++ app/javascript/packs/application.js
@@ -0,0 +1,4 @@
+import 'core-js/stable'
+import 'regenerator-runtime/runtime'
+
+import '../../assets/javascripts/components/products_search.js'
