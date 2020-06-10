a6b3492a66d9ce3a52f2e5f04ed439c888d05aae
Pauline <pauline.eustachy@gmail.com>
2020-06-10 05:58:51 +0200

    add navabr animation on hover for the homepage

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -10,4 +10,11 @@ module ProductsHelper
   def unslug(sub_category)
     sub_category.gsub('-', ' ')
   end
+
+  def sub_categories_by_weight(category)
+    sub_categories = Product.where(category: category).pluck(:sub_category)
+    sub_categories_counts = Hash.new(0)
+    sub_categories.each {|sub_category| sub_categories_counts[sub_category] += 1}
+    sub_categories_counts.sort_by {|k, v| -v}.to_h
+  end
 end
