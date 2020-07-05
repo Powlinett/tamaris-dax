fc188c597f2c8a4cb38aaf00f4686a4c49a7cefe
Pauline <pauline.eustachy@gmail.com>
2020-07-05 05:01:08 +0200

    created mails layouts when a booking is recorded, confirmed and canceled

--- app/mailers/booking_mailer.rb
+++ app/mailers/booking_mailer.rb
@@ -1,4 +1,6 @@
 class BookingMailer < ApplicationMailer
+  add_template_helper(ProductsHelper)
+
   before_action :set_booking
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
@@ -6,15 +8,24 @@ class BookingMailer < ApplicationMailer
   #   en.booking_mailer.registration.subject
   #
   def registration
-    mail(to: @booker.email, subject: 'Votre réservation sur Tamaris-Dax')
+    mail(to: @booker.email, subject: 'Votre réservation sur Tamaris-Dax') do |format|
+      format.html { render :registration }
+      format.text { render :registration }
+    end
   end
 
   def confirmation
-    mail(to: @booker.email, subject: 'Confirmation de votre réservation')
+    mail(to: @booker.email, subject: 'Confirmation de votre réservation') do |format|
+      format.html { render :confirmation }
+      format.text { render :confirmation }
+    end
   end
 
   def cancel
-    mail(to: @booker.email, subject: "Votre réservation n'a pas pu être confirmée")
+    mail(to: @booker.email, subject: "Votre réservation n'a pas pu être confirmée") do |format|
+      format.html { render :cancel }
+      format.text { render :cancel }
+    end
   end
 
   private
